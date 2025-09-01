96ca46c00e1a440ad44d21169a8da366a08e3899
NiklasFelhauer <nfelhauer@gmail.com>
2025-09-01 13:06:19 +0200

    Addresse und Topic angepasst in der App

--- 00_Android_App/app/src/main/java/com/hka/objectdetection/MainViewModel.kt
+++ 00_Android_App/app/src/main/java/com/hka/objectdetection/MainViewModel.kt
@@ -42,8 +42,8 @@ class MainViewModel : ViewModel() {
 
     // MQTT Client
     private var mqttClient: MqttClient? = null
-    private val brokerUri = "tcp://192.168.1.12:1883"
-    private val topic = "mein/test/topic"
+    private val brokerUri = "tcp://192.168.188.26:1883"
+    private val topic = "brauanlage/data"
 
     private val _mqttConnected = MutableLiveData<Boolean>(false)
     val mqttConnected: LiveData<Boolean> get() = _mqttConnected
